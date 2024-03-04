99319bcca93e480589d7a47f66d2b49a53daf4bd
Loong Yong En <yongen.loong@aelf.io>
2024-03-04 19:34:33 +0800

    init

---
+++ src/App.tsx
@@ -0,0 +1,83 @@
+import "./App.css";
+
+import { useMemo, useState } from "react";
+
+import breed from "./assets/breed.json";
+import clothes from "./assets/clothes.json";
+import eyes from "./assets/eyes.json";
+import hats from "./assets/hats.json";
+import mouth from "./assets/mouth.json";
+import pet from "./assets/pet.json";
+
+const randomElement = (myArray: Array<string>) =>
+  myArray[Math.floor(Math.random() * myArray.length)];
+
+function App() {
+  const [numGen, setNumGen] = useState(1000);
+  const [textarea, setTextarea] = useState("");
+  const [format, setFormat] = useState(
+    "Create a %breed% cat, wearing %clothes% clothes, %eyes%, wearing %hat%, with %mouth% mouth, with %pet% pet"
+  );
+
+  const generate = () => {
+    let testcases = [];
+
+    for (let i = 0; i < numGen; i++) {
+      const _breed = randomElement(breed);
+      const _clothes = randomElement(clothes);
+      const _eyes = randomElement(eyes);
+      const _hats = randomElement(hats);
+      const _mouth = randomElement(mouth);
+      const _pet = randomElement(pet);
+
+      const testcase = format
+        .replace(/%breed%/g, _breed)
+        .replace(/%clothes%/g, _clothes)
+        .replace(/%eyes%/g, _eyes)
+        .replace(/%hat%/g, _hats)
+        .replace(/%mouth%/g, _mouth)
+        .replace(/%pet%/g, _pet);
+
+      testcases.push(testcase);
+    }
+
+    setTextarea(testcases.join("\n"));
+  };
+
+  return (
+    <>
+      <div>
+        <label>
+          Format
+          <textarea
+            value={format}
+            onChange={(e) => setFormat(e.target.value)}
+          />
+        </label>
+        <label>
+          Number of generations
+          <input
+            type="number"
+            min={1}
+            value={numGen}
+            onChange={(e) => setNumGen(Number(e.target.value))}
+          />
+        </label>
+        <br />
+        <button onClick={generate}>Generate</button>
+        <br />
+        <textarea rows={30} value={textarea} />
+        <br />
+        <button
+          onClick={() => {
+            navigator.clipboard.writeText(textarea);
+          }}
+        >
+          Copy
+        </button>
+      </div>
+    </>
+  );
+}
+
+export default App;
